6d61a9c8083043b6a03f30d2fd08a95c8c5bf732
pchien1901 <phamminhchien1901@gmail.com>
2023-05-18 17:25:23 +0700

    update create post page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -32,12 +32,14 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import * as Yup from "yup";
 import FilesDropzone from "../components/Posts/FilesDropzone";
+import { speciesGenre } from "./gen";
 
 const CreatePost = () => {
   const [province, setProvince] = useState([]);
   const [district, setDistrict] = useState([]);
   const [commune, setCommune] = useState([]);
   const [endDatee, setEndDatee] = useState(moment().add(7, "days").toDate());
+  const [genre, setGenre] = useState([]);
   const navigate = useNavigate();
   const toast = useToast();
 
@@ -85,14 +87,21 @@ const CreatePost = () => {
     setEndDatee(date);
   };
 
+  const handleSpeciesChange = (e) => {
+    const speciesName = e.target.value;
+    const result = speciesGenre.find((s) => s.Name === speciesName);
+    setGenre(result.Genre);
+  }
+
   const validationSchema = Yup.object().shape({
     title: Yup.string().required("Vui lòng nhập tiêu đề cho bài viết"),
     species: Yup.string()
       .required("Vui lòng chọn trường này")
       .oneOf(
-        ["Chó", "Mèo", "Chuột Hamster", "Khác"],
+        ["Chó", "Mèo", "Chim", "Gà", "Chuột Hamster", "Khác"],
         "Vui lòng chọn trường này"
       ),
+    genre: Yup.string().required("Vui lòng chọn trường này"),
     gender: Yup.string()
       .required("Vui lòng chọn trường này")
       .oneOf(["Đực", "Cái"], "Vui lòng chọn trường này"),
@@ -140,6 +149,7 @@ const CreatePost = () => {
             commune: "",
             address: "",
             species: "Mèo",
+            genre: "",
             quantity: "",
             gender: "",
             price: "",
@@ -151,7 +161,8 @@ const CreatePost = () => {
             endDate: moment().add(7, "days").toDate().toString(),
           }}
           onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-            handleSubmit(values);
+            //handleSubmit(values);
+            console.log(values);
           }}
           validationSchema={validationSchema}
         >
@@ -180,33 +191,69 @@ const CreatePost = () => {
                 )}
               </Field>
 
-              <Field name="species">
-                {({ field, form }) => (
-                  <FormControl
-                    isRequired
-                    isInvalid={form.errors.species && form.touched.species}
-                    mb={"4"}
-                  >
-                    <FormLabel>Loại thú cưng</FormLabel>
-                    <Select
-                      onChange={(e) =>
-                        form.setValues({
-                          ...form.values,
-                          species: e.target.value,
-                        })
-                      }
-                    >
-                      {/* <option value={''} disabled hidden selected >Xin mời chọn</option> */}
-                      <option value={"Mèo"}>Mèo</option>
-                      <option value={"Chó"}>Chó</option>
-                      <option value={"Chuột Hamster"}>Chuột Hamster</option>
-                      <option value={"Khác"}>Thú cưng khác</option>
-                    </Select>
-                    <FormErrorMessage>{form.errors.species}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-
+              <Grid templateColumns={"repeat(2, 1fr)"} gap={4}>
+                <GridItem>
+                  <Field name="species">
+                    {({ field, form }) => (
+                      <FormControl
+                        isRequired
+                        isInvalid={form.errors.species && form.touched.species}
+                        mb={"4"}
+                      >
+                        <FormLabel>Loài</FormLabel>
+                        <Select
+                          placeholder='Chọn loài'
+                          onChange={(e) => {
+                            handleSpeciesChange(e);
+                            form.setValues({
+                              ...form.values,
+                              species: e.target.value,
+                            });
+                          }}
+                        >
+                          {/* <option value={''} disabled hidden selected >Xin mời chọn</option> */}
+                          {speciesGenre.map((s) => (
+                            <option key={s.Id} value={s.Name}>
+                              {s.Name}
+                            </option>
+                          ))}
+                        </Select>
+                        <FormErrorMessage>{form.errors.species}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                </GridItem>
+                <GridItem>
+                  <Field name="genre">
+                    {({ field, form }) => (
+                      <FormControl
+                        isRequired
+                        isInvalid={form.errors.genre && form.touched.genre}
+                        mb={"4"}
+                      >
+                        <FormLabel>Giống</FormLabel>
+                        <Select
+                          placeholder='Chọn giống'
+                          onChange={(e) => {
+                            form.setValues({
+                              ...form.values,
+                              genre: e.target.value,
+                            })
+                          }}
+                        >
+                          {/* <option value={''} disabled hidden selected >Xin mời chọn</option> */}
+                          {genre.map((g) => (
+                            <option key={g.Id} value={g.Name}>
+                              {g.Name}
+                            </option>
+                          ))}
+                        </Select>
+                        <FormErrorMessage>{form.errors.species}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                </GridItem>
+              </Grid>
               <Field name="quantity">
                 {({ field, form }) => (
                   <FormControl
---
+++ src/pages/gen.js
@@ -0,0 +1,361 @@
+export const speciesGenre = [
+  {
+    "Id":"01",
+    "Name":"Chó",
+    "Genre":[
+      {
+        "Id":"0101",
+        "Name":"Chó Alaska"
+      },
+      {
+        "Id":"0102",
+        "Name":"Chó Becgie"
+      },
+      {
+        "Id":"0103",
+        "Name":"Chó Bull"
+      },
+      {
+        "Id":"0104",
+        "Name":"Chó Becgie"
+      },
+      {
+        "Id":"0105",
+        "Name":"Chó Bắc Kinh"
+      },
+      {
+        "Id":"0106",
+        "Name":"Chó Chihuahua"
+      },
+      {
+        "Id":"0107",
+        "Name":"Chó Chowchow"
+      },
+      {
+        "Id":"0108",
+        "Name":"Chó Cocker"
+      },
+      {
+        "Id":"0109",
+        "Name":"Chó Corgi"
+      },
+      {
+        "Id":"0110",
+        "Name":"Chó Cỏ"
+      },
+      {
+        "Id":"0111",
+        "Name":"Chó Becgie"
+      },
+      {
+        "Id":"0112",
+        "Name":"Chó Golden"
+      },
+      {
+        "Id":"0113",
+        "Name":"Chó Gấu Bắc Hà"
+      },
+      {
+        "Id":"0114",
+        "Name":"Chó Husky"
+      },
+      {
+        "Id":"0115",
+        "Name":"Chó Nhật"
+      },
+      {
+        "Id":"0116",
+        "Name":"Chó Pitbull"
+      },
+      {
+        "Id":"0117",
+        "Name":"Chó Poodle"
+      },
+      {
+        "Id":"0118",
+        "Name":"Chó Bug"
+      },
+      {
+        "Id":"0119",
+        "Name":"Chó Shiba"
+      },
+      {
+        "Id":"0120",
+        "Name":"Chó Đốm"
+      },
+      {
+        "Id":"0121",
+        "Name":"Khác"
+      }
+    ]
+  },
+  {
+    "Id":"02",
+    "Name":"Mèo",
+    "Genre":[
+      {
+        "Id":"0201",
+        "Name":"Mèo Anh Lông Ngắn"
+      },
+      {
+        "Id":"0202",
+        "Name":"Mèo Anh Lông Dài"
+      },
+      {
+        "Id":"0203",
+        "Name":"Mèo Ba Tư"
+      },
+      {
+        "Id":"0204",
+        "Name":"Mèo Bengal"
+      },
+      {
+        "Id":"0205",
+        "Name":"Mèo Golden"
+      },
+      {
+        "Id":"0206",
+        "Name":"Mèo Maine Coon"
+      },
+      {
+        "Id":"0207",
+        "Name":"Mèo Munchkin"
+      },
+      {
+        "Id":"0208",
+        "Name":"Mèo Mướp"
+      },
+      {
+        "Id":"0209",
+        "Name":"Mèo Mỹ"
+      },
+      {
+        "Id":"0210",
+        "Name":"Mèo Nga"
+      },
+      {
+        "Id":"0211",
+        "Name":"Mèo Ragdoll"
+      },
+      {
+        "Id":"0212",
+        "Name":"Mèo Scottish"
+      },
+      {
+        "Id":"0213",
+        "Name":"Mèo Sphynx"
+      },
+      {
+        "Id":"0214",
+        "Name":"Mèo Ta"
+      },
+      {
+        "Id":"0215",
+        "Name":"Mèo Tam Thể"
+      },
+      {
+        "Id":"0216",
+        "Name":"Mèo Tuxedo"
+      },
+      {
+        "Id":"0217",
+        "Name":"Mèo Xiêm"
+      },
+      {
+        "Id":"0218",
+        "Name":"Khác"
+      }
+    ]
+  },
+  {
+    "Id":"03",
+    "Name":"Chim",
+    "Genre":[
+      {
+        "Id":"0301",
+        "Name":"Chim Bách Thanh"
+      },
+      {
+        "Id":"0302",
+        "Name":"Chim Bồ Câu"
+      },
+      {
+        "Id":"0303",
+        "Name":"Chim Chào Mào"
+      },
+      {
+        "Id":"0304",
+        "Name":"Chim Chìa Vôi"
+      },
+      {
+        "Id":"0305",
+        "Name":"Chim Chích Choè"
+      },
+      {
+        "Id":"0306",
+        "Name":"Chim Cu Gáy"
+      },
+      {
+        "Id":"0307",
+        "Name":"Chim Hoàng Yến"
+      },
+      {
+        "Id":"0308",
+        "Name":"Chim Hoạ Mi"
+      },
+      {
+        "Id":"0309",
+        "Name":"Chim Hồng Tước"
+      },
+      {
+        "Id":"0310",
+        "Name":"Chim Khướu"
+      },
+      {
+        "Id":"0311",
+        "Name":"Chim Phượng Hoàng"
+      },
+      {
+        "Id":"0312",
+        "Name":"Chim Quế Lâm"
+      },
+      {
+        "Id":"0313",
+        "Name":"Chim Sáo"
+      },
+      {
+        "Id":"0314",
+        "Name":"Chim Sâu"
+      },
+      {
+        "Id":"0315",
+        "Name":"Chim Sơn Ca"
+      },
+      {
+        "Id":"0316",
+        "Name":"Chim Sẻ"
+      },
+      {
+        "Id":"0317",
+        "Name":"Chim Thanh Tước"
+      },
+      {
+        "Id":"0318",
+        "Name":"Chim Tiểu Mi"
+      },
+      {
+        "Id":"0319",
+        "Name":"Chim Trích Cồ"
+      },
+      {
+        "Id":"0320",
+        "Name":"Chim Vàng Anh"
+      },
+      {
+        "Id":"0321",
+        "Name":"Chim Vành Khuyên"
+      },
+      {
+        "Id":"0322",
+        "Name":"Chim Vẹt"
+      },
+      {
+        "Id":"0323",
+        "Name":"Khác"
+      }
+    ]
+  },
+  {
+    "Id":"04",
+    "Name":"Gà",
+    "Genre":[
+      {
+        "Id":"0401",
+        "Name":"Gà Chọi"
+      },
+      {
+        "Id":"0402",
+        "Name":"Gà Brahma"
+      },
+      {
+        "Id":"0403",
+        "Name":"Gà Bướm"
+      },
+      {
+        "Id":"0404",
+        "Name":"Gà Mặt Quỷ"
+      },
+      {
+        "Id":"0405",
+        "Name":"Gà Quý Phi"
+      },
+      {
+        "Id":"0406",
+        "Name":"Gà Serama"
+      },
+      {
+        "Id":"0407",
+        "Name":"Gà Ta"
+      },
+      {
+        "Id":"0408",
+        "Name":"Gà Tre"
+      },
+      {
+        "Id":"0409",
+        "Name":"Gà Vảy Cá"
+      },
+      {
+        "Id":"0410",
+        "Name":"Gà Vảy Rồng"
+      },
+      {
+        "Id":"0411",
+        "Name":"Gà Ác"
+      },
+      {
+        "Id":"0412",
+        "Name":"Gà Đông Tảo"
+      },
+      {
+        "Id":"0413",
+        "Name":"Khác"
+      }
+    ]
+  },
+  {
+    "Id":"05",
+    "Name":"Chuột Hamster",
+    "Genre":[
+      {
+        "Id":"0501",
+        "Name":"Chuột Hamster Bear"
+      },
+      {
+        "Id":"0502",
+        "Name":"Chuột Hamster Campbell"
+      },
+      {
+        "Id":"0503",
+        "Name":"Chuột Hamster Robo"
+      },
+      {
+        "Id":"0504",
+        "Name":"Chuột Hamster Trung Quốc"
+      },
+      {
+        "Id":"0505",
+        "Name":"Chuột Hamster Winter White"
+      },
+      {
+        "Id":"0506",
+        "Name":"Khác"
+      }
+    ]
+  },
+  {
+    "Id":"06",
+    "Name":"Khác",
+    "Genre":[]
+  }
+]
